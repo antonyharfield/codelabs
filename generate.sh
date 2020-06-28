5d4ba257212a0e80f546ca45e342c4e3d5846cbf
Antony Harfield <antonyharfield@gmail.com>
2020-06-28 15:29:03 +0700

    Remove prefix

--- generate.sh
+++ generate.sh
@@ -4,7 +4,7 @@ claat="./claat-2.2.0"
 outputdir="public"
 
 if [ $# -gt 0 ]; then
-    $claat export -f html -o $outputdir -prefix https://ant-codelabs.web.app "$@"
+    $claat export -f html -o $outputdir "$@"
 else
-    $claat update -f html -o $outputdir -prefix https://ant-codelabs.web.app
+    $claat update -f html -o $outputdir
 fi
